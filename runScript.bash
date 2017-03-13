010bed6c484ab6ad88ebd19dfff8c20cb49ccc08
Marco Bender <marco.bender@inform-software.com>
2017-03-13 08:23:22 +0100

    Removed increasedLookahead

--- runScript.bash
+++ runScript.bash
@@ -4,7 +4,3 @@ for file in testFiles/*
 do
 	./optimizer.exe optimizer.par "${file}" outsDFS/"$(basename "$file").out" > logsDFS/"$(basename "$file").log"
 done
-for file in testFiles/increasedLookahead/*
-do
-	./optimizer.exe optimizer.par "${file}" outsDFS/increasedLookahead/"$(basename "$file").out" > logsDFS/increasedLookahead/"$(basename "$file").log"
-done
