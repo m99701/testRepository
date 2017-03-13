d0b280bb831340a75bfb49debcf0d68f9f5b306b
Marco Bender <marco.bender@inform-software.com>
2017-03-13 08:23:53 +0100

    Added testFiles2

--- runScript.bash
+++ runScript.bash
@@ -4,3 +4,7 @@ for file in testFiles/*
 do
 	./optimizer.exe optimizer.par "${file}" outsDFS/"$(basename "$file").out" > logsDFS/"$(basename "$file").log"
 done
+for file in testFiles2/*
+do
+	./optimizer.exe optimizer.par "${file}" outsDFS/"$(basename "$file").out" > logsDFS/"$(basename "$file").log"
+done
